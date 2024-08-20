4668217d0e04450f5c8ad37e4768aca17be4a843
Prashidha0O1 <89299485+Prashidha0O1@users.noreply.github.com>
2024-08-20 14:36:47 +0545

    Reading documentations is tiring as hell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use image::io::Reader as ImageReader; // it is sort of conflicting with standard io read "std::io::Read"
+
+fn resizee()
+fn main()  -> Result <(), ()>{
+
+    let imge: Reader<BufReader<File>> = ImageReader::open(path: "")?.decode()?;
     println!("Hello, world!");
 }
